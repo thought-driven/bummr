3490631c8eaf01f9ad899d940c4ade4bc45f4c7c
Lee Pender <lpender@gmail.com>
2016-11-08 17:34:23 +0000

    Update Code Climate reporting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,13 @@
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+require "simplecov"
+
+# https://circleci.com/docs/code-coverage/
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
+
+SimpleCov.start
+
 require 'pry'
 require 'bummr'
 require 'colorize'
